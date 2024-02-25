7836fc748a03cbf98de1aa627e9ff0ebff4fec28
Maddasbot <130859162+Maddasbot@users.noreply.github.com>
2024-02-25 23:37:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
+  git clone https://github.com/Maddasbot/DRNBLASTERS-/tree/Tech_VJ /DRNBLASTERS- 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
+  git clone $UPSTREAM_REPO /DRNBLASTERS-
 fi
-cd /VJ-FILTER-BOT 
+cd /DRNBLASTERS- 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
