2087a607b32bebdaa240fc08d9d94402dc90f3d9
Maddasbot <130859162+Maddasbot@users.noreply.github.com>
2024-03-08 00:12:46 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1136,5 +1136,5 @@ async def removetutorial(bot, message):
 async def stop_button(bot, message):
     msg = await bot.send_message(text="**🔄 𝙿𝚁𝙾𝙲𝙴𝚂𝚂𝙴𝚂 𝚂𝚃𝙾𝙿𝙴𝙳. 𝙱𝙾𝚃 𝙸𝚂 𝚁𝙴𝚂𝚃𝙰𝚁𝚃𝙸𝙽𝙶...**", chat_id=message.chat.id)       
     await asyncio.sleep(3)
-    await msg.edit("**✅️ 𝙱𝙾𝚃 𝙸𝚂 𝚁𝙴𝚂𝚃𝙰𝚁𝚃𝙴𝙳. 𝙽𝙾𝚆 𝚈𝙾𝚄 𝙲𝙰𝙽 𝚄𝚂𝙴 𝙼𝙴**")
+    await msg.edit("**✅️ 𝙱𝙾𝚃 𝙸𝚂 𝚁𝙴𝚂𝚃𝙰𝚁𝚃𝙴𝙳. 𝙼𝚊𝚍𝚎 𝚋𝚢 𝚅𝙳𝙼𝙾𝚅𝙸𝙴𝚉**")
     os.execl(sys.executable, sys.executable, *sys.argv)
