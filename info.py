e6b2cbcbb52075cf9493ad2d26b6ac17e6210c71
Maddasbot <130859162+Maddasbot@users.noreply.github.com>
2024-03-09 17:10:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -54,9 +54,9 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'http://OmegaLinks.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', '3d8398c5b204e2e85883ed3f0c1d18503c5c344b')
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
+SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
+SHORTLINK_API = environ.get('SHORTLINK_API', '')
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
