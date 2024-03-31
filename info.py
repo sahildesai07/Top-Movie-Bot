06c8c3fd23369c3e65f7e335bba945bda7a9ad0f
Maddasbot <130859162+Maddasbot@users.noreply.github.com>
2024-04-01 02:58:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -121,9 +121,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://drnblasters.onrender.com/".format(FQDN)
+    URL = "https://drnblasters-luf7.onrender.com/".format(FQDN)
 else:
-    URL = "https://drnblasters.onrender.com/".format(FQDN)
+    URL = "https://drnblasters-luf7.onrender.com/".format(FQDN)
 
 
 
