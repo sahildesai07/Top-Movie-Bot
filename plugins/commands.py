d3f1b760957defcf82b9bdcf7f6810c378b4ef9d
mediashow203 <159476387+mediashow203@users.noreply.github.com>
2024-04-01 14:14:20 -0400

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -91,6 +91,7 @@ async def start(client, message):
         if message.command[1] != "subscribe":
             try:
                 kk, file_id = message.command[1].split("_", 1)
+                pre = 'checksubp' if kk == 'filep' else 'checksub'
                 btn.append([InlineKeyboardButton("↻ Tʀʏ Aɢᴀɪɴ", callback_data=f"checksub#{kk}#{file_id}")])
             except (IndexError, ValueError):
                 btn.append([InlineKeyboardButton("↻ Tʀʏ Aɢᴀɪɴ", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
