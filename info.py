c0acdeb252714fd07fdf7f8c75a302d905a92ce7
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-04-02 06:51:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -111,7 +111,7 @@ SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
 MULTI_CLIENT = False
-name = str(environ.get('name', 'LazyPrincess'))
+name = str(environ.get('name', 'TechVJBot'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if 'DYNO' in environ:
     ON_HEROKU = True
