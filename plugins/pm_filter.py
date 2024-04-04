cd304def9f612dae368010cc359a7b5933647b86
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-04-04 20:22:31 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -79,7 +79,7 @@ async def pm_text(bot, message):
     user = message.from_user.first_name
     user_id = message.from_user.id
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
-    if user_id in ADMINS: return # ignore admins
+  #  if user_id in ADMINS: return # ignore admins
     if PM_SEARCH == True:
         kd = await manual_filters(client, message)
         if kd == False:
