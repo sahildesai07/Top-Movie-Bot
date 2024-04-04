401f7eac6233b25be1bf90905924fa51153fec34
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-04-04 06:29:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,7 +1,8 @@
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
-
+# premium code credit Git - @Rishikesh-Sharma09
+# referal code credit Git - @VJBots
 
 import os
 import logging
