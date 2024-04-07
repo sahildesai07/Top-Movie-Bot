fba89723ab1320407a81394a7c49f9c251a0adf6
venomdarshu7 <138948660+venomdarshu7@users.noreply.github.com>
2024-04-08 03:47:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -30,7 +30,7 @@ BATCH_FILES = {}
 
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
-    await message.react(emoji="❤️")
+    await msg.react(choice(Telegram.EMOJIS))
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [[
                     InlineKeyboardButton('🔰 Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ 🔰', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
