43e88c57af5f4b2bccb2a17f2c003f5041e8282e
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-04-14 10:43:55 +0530

    Update sample.py

--- plugins/sample.py
+++ plugins/sample.py
@@ -1,1 +1,3 @@
-# Don't Remove Credit @VJ_Bot Subscribe YouTube Channel For Amazing Bot @Tech_VJ Ask Doubt on telegram @KingVJ01 -->
+# Don't Remove Credit @VJ_Botz 
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ 
+# Ask Doubt on telegram @KingVJ01
