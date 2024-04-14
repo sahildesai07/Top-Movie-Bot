e79a1b3f2a5f6218f952e6648593f2d68f3f3a1f
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-04-14 16:02:19 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,7 +18,7 @@ from pyrogram.types import *
 from database.ia_filterdb import Media, get_file_details, unpack_new_file_id, get_bad_files
 from database.users_chats_db import db, delete_all_referal_users, get_referal_users_count, get_referal_all_users, referal_add_user
 from info import CHANNELS, ADMINS, SHORTLINK_MODE, PREMIUM_AND_REFERAL_MODE, STREAM_MODE, AUTH_CHANNEL, OWNER_USERNAME, REFERAL_PREMEIUM_TIME, REFERAL_COUNT, PAYMENT_TEXT, PAYMENT_QR, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT, CHNL_LNK, GRP_LNK, REQST_CHANNEL, SUPPORT_CHAT_ID, SUPPORT_CHAT, MAX_B_TN, VERIFY, SHORTLINK_API, SHORTLINK_URL, TUTORIAL, IS_TUTORIAL, URL
-from utils import get_settings, is_subscribed, get_size, is_subscribed, save_group_settings, temp, verify_user, check_token, check_verification, get_token, get_shortlink, get_tutorial, get_seconds
+from utils import get_settings, pub_is_subscribed, get_size, is_subscribed, save_group_settings, temp, verify_user, check_token, check_verification, get_token, get_shortlink, get_tutorial, get_seconds
 from database.connections_mdb import active_connection
 # from plugins.pm_filter import ENABLE_SHORTLINK
 import re, asyncio, os, sys
@@ -392,7 +392,7 @@ async def start(client, message):
         settings = await get_settings(chat_id)
         if settings['fsub'] != None:
             try:
-                btn = await is_subscribed(client, message, int(settings['fsub']))
+                btn = await pub_is_subscribed(client, message, int(settings['fsub']))
                 if btn:
                     btn.append(
                         [InlineKeyboardButton("♻️ Try Again ♻️", callback_data=f"tryagain#{chat_id}#{data}")]
@@ -430,7 +430,7 @@ async def start(client, message):
         settings = await get_settings(chat_id)
         if settings['fsub'] != None:
             try:
-                btn = await is_subscribed(client, message, int(settings['fsub']))
+                btn = await pub_is_subscribed(client, message, int(settings['fsub']))
                 if btn:
                     btn.append(
                         [InlineKeyboardButton("♻️ Try Again ♻️", callback_data=f"tryagain#{chat_id}#{data}")]
@@ -534,7 +534,7 @@ async def start(client, message):
         settings = await get_settings(chat_id)
         if settings['fsub'] != None:
             try:
-                btn = await is_subscribed(client, message, int(settings['fsub']))
+                btn = await pub_is_subscribed(client, message, int(settings['fsub']))
                 if btn:
                     btn.append(
                         [InlineKeyboardButton("♻️ Try Again ♻️", callback_data=f"tryagain#{chat_id}#{data}")]
