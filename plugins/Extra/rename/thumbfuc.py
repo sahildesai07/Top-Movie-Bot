dbca57de6dfccbac365f3f141096573f4eea5dfd
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-04-16 22:25:20 +0530

    Update thumbfuc.py

--- plugins/Extra/rename/thumbfuc.py
+++ plugins/Extra/rename/thumbfuc.py
@@ -9,9 +9,9 @@ from database.users_chats_db import db
 async def viewthumb(client, message):    
     thumb = await db.get_thumbnail(message.from_user.id)
     if thumb:
-       await client.send_photo(
-	   chat_id=message.chat.id, 
-	   photo=thumb)
+        await client.send_photo(
+	    chat_id=message.chat.id, 
+	    photo=thumb)
     else:
         await message.reply_text("😔**Sorry ! No thumbnail found...**😔") 
 		
@@ -24,8 +24,8 @@ async def removethumb(client, message):
 async def addthumbs(client, message):
     thumb = await client.ask(message.chat.id, "**Send me your thumbnail**")
     if thumb.media and thumb.media == enums.MessageMediaType.PHOTO:
-	await db.set_thumbnail(message.from_user.id, file_id=thumb.photo.file_id)                
-        await message.reply("**Thumbnail saved successfully**✅️")
+	await db.set_thumbnail(message.from_user.id, file_id=thumb.photo.file_id)
+	await message.reply("**Thumbnail saved successfully**✅️")
     else:
 	await message.reply("**This is not a picture**")
 	
