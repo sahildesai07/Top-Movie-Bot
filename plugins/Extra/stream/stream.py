d72031ae375b81ba3e199aaa3e46ac5a223b9dca
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-04-17 18:00:18 +0530

    Update stream.py

--- plugins/Extra/stream/stream.py
+++ plugins/Extra/stream/stream.py
@@ -1,6 +1,8 @@
 from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery
-from info import STREAM_MODE
+from info import STREAM_MODE, URL, LOG_CHANNEL
+from urllib.parse import quote_plus
+from TechVJ.util.file_properties import get_name, get_hash, get_media_file_size
 import humanize
 import random
 
@@ -11,8 +13,26 @@ async def stream_start(client, message):
     msg = await client.ask(message.chat.id, "**Now send me your file/video to get stream and download link**")
     if not msg.media:
         return await message.reply("**Please send me supported media.**")
-    if msg.media in [enums.MessageMediaType.VIDEO, enums.MessageMediaType.DOCUMENT, enums.MessageMediaType.AUDIO]:
+    if msg.media in [enums.MessageMediaType.VIDEO, enums.MessageMediaType.DOCUMENT]:
         file = getattr(msg, msg.media.value)
         filename = file.file_name
         filesize = humanize.naturalsize(file.file_size) 
         fileid = file.file_id
+        user_id = message.from_user.id
+        username =  message.from_user.mention 
+
+        log_msg = await client.send_cached_media(
+            chat_id=LOG_CHANNEL,
+            file_id=fileid,
+        )
+        fileName = {quote_plus(get_name(log_msg))}
+        stream = f"{URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
+        download = f"{URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
+ 
+        await log_msg.reply_text(
+            text=f"•• ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴇᴅ ꜰᴏʀ ɪᴅ #{user_id} \n•• ᴜꜱᴇʀɴᴀᴍᴇ : {username} \n\n•• ᖴᎥᒪᗴ Nᗩᗰᗴ : {fileName}",
+            quote=True,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🚀 Fast Download 🚀", url=download),  # we download Link
+                                                InlineKeyboardButton('🖥️ Watch online 🖥️', url=stream)]])  # web stream Link
+        )
