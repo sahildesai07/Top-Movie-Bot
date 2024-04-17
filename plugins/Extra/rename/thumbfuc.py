2b4e7227ef84ca4ad9902ed0ad930fe2d65bb7dd
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-04-17 06:17:46 +0530

    Update thumbfuc.py

--- plugins/Extra/rename/thumbfuc.py
+++ plugins/Extra/rename/thumbfuc.py
@@ -4,9 +4,12 @@
 
 from pyrogram import Client, filters, enums
 from database.users_chats_db import db
+from info import RENAME_MODE
 
 @Client.on_message(filters.private & filters.command(['view_thumb']))
-async def viewthumb(client, message):    
+async def viewthumb(client, message):
+    if RENAME_MODE == False:
+        return 
     thumb = await db.get_thumbnail(message.from_user.id)
     if thumb:
         await client.send_photo(chat_id=message.chat.id, photo=thumb)
@@ -15,11 +18,15 @@ async def viewthumb(client, message):
 
 @Client.on_message(filters.private & filters.command(['del_thumb']))
 async def removethumb(client, message):
+    if RENAME_MODE == False:
+        return 
     await db.set_thumbnail(message.from_user.id, file_id=None)
     await message.reply_text("**Thumbnail deleted successfully ✅️**")
 
 @Client.on_message(filters.private & filters.command(['set_thumb']))
 async def addthumbs(client, message):
+    if RENAME_MODE == False:
+        return 
     thumb = await client.ask(message.chat.id, "**Send me your thumbnail**")
     if thumb.media and thumb.media == enums.MessageMediaType.PHOTO:
         await db.set_thumbnail(message.from_user.id, file_id=thumb.photo.file_id)
