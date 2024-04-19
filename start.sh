b25f122eae871c5cd1a7174b9580f2232b09e643
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-04-19 14:19:11 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,16 @@
+# Don't Remove Credit @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/VJBots/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
+fi
+cd /VJ-FILTER-BOT 
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
