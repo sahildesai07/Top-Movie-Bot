a8d2a05a36fdfc91e8403cfa263003f75c4d2bb7
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-04-23 11:53:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -35,8 +35,10 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL')
-auth_grp = environ.get('AUTH_GROUP')
+
+# auth_channel means force subscribe channel.
+auth_channel = environ.get('AUTH_CHANNEL', '') # give your force subscribe channel id here.
+auth_grp = environ.get('AUTH_GROUP', '') # give your force subscribe group id here.
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 support_chat_id = environ.get('SUPPORT_CHAT_ID', '')
