a2160aabc5c2b81858b4fc12b87b5d464e7137db
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-04-28 07:34:49 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -17,9 +17,7 @@ async def pm_broadcast(bot, message):
         return await message.reply_text("<b>Reply this command to your broadcast message.</b>")
     users = await db.get_all_users()
     b_msg = message.reply_to_message
-    sts = await message.reply_text(
-        text='Broadcasting your messages...'
-    )
+    sts = await message.reply_text('Broadcasting your messages...')
     start_time = time.time()
     total_users = await db.total_users_count()
     done = 0
