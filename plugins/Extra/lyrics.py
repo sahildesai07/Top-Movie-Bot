2ff6513edac631dbcb60f183af3c74215516b603
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-04-30 13:46:18 +0530

    Update lyrics.py

--- plugins/Extra/lyrics.py
+++ plugins/Extra/lyrics.py
@@ -15,32 +15,31 @@ API = "https://apis.xditya.me/lyrics?song="
 
 @Client.on_message(filters.text & filters.command(["lyrics"]))
 async def sng(bot, message):
-        if not message.reply_to_message:
-          await message.reply_text("Please reply to a message")
-        else:          
-          mee = await message.reply_text("`Searching 🔎`")
-          song = message.reply_to_message.text
-          chat_id = message.from_user.id
-          rpl = lyrics(song)
-          await mee.delete()
-          try:
+    vj = await bot.ask(chat_id=message.from_user.id, text="Now send me your song name.")
+    if vj.text:
+        mee = await vj.reply_text("`Searching 🔎`")
+        song = vj.text
+        chat_id = message.from_user.id
+        rpl = lyrics(song)
+        await mee.delete()
+        try:
             await mee.delete()
-            await bot.send_message(chat_id, text = rpl, reply_to_message_id = message.id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs ", url = f"t.me/vj_bots")]]))
-          except Exception as e:                            
-             await message.reply_text(f"I Can't Find A Song With `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url = f"t.me/vj_bots")]]))
+            await bot.send_message(chat_id, text = rpl, reply_to_message_id = message.id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs ", url = f"t.me/vj_botz")]]))
+        except Exception as e:                            
+            await vj.reply_text(f"I Can't Find A Song With `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url = f"t.me/vj_botz")]]))
 
 
 def search(song):
-        r = requests.get(API + song)
-        find = r.json()
-        return find
+    r = requests.get(API + song)
+    find = r.json()
+    return find
        
 def lyrics(song):
-        fin = search(song)
-        text = f'**🎶 Sᴜᴄᴄᴇꜱꜰᴜʟʟy Exᴛʀᴀᴄᴛᴇᴅ Lyɪʀɪᴄꜱ Oꜰ {song}**\n\n'
-        text += f'`{fin["lyrics"]}`'
-        text += '\n\n\n**Made By Artificial Intelligence**'
-        return text
+    fin = search(song)
+    text = f'**🎶 Sᴜᴄᴄᴇꜱꜰᴜʟʟy Exᴛʀᴀᴄᴛᴇᴅ Lyɪʀɪᴄꜱ Oꜰ {song}**\n\n'
+    text += f'`{fin["lyrics"]}`'
+    text += '\n\n\n**Made By Artificial Intelligence**'
+    return text
 
 
 
