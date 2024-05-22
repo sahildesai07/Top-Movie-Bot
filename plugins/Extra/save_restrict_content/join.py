2cdb8515c64670d95394d2b9ce798d3203398a42
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-05-22 12:40:53 +0530

    Update and rename TechVJ to join.py

---
+++ plugins/Extra/save_restrict_content/join.py
@@ -0,0 +1,22 @@
+from pyrogram.errors import FloodWait, InviteHashInvalid, InviteHashExpired, UserAlreadyParticipant
+from pyrogram import Client, filters
+
+@Client.on_message(filters.private & filters.command('join'))
+async def join(bot, message):
+    invite_link = await bot.ask(message.chat.id, "**Now Send Me Your Channel Invite Link From You Want To Save Restricted Content.**")
+    if not 't.me/+' in invite_link:
+        return 
+    try:
+    #    client = ......userbot
+        await client.join_chat(invite_link)
+        return await message.reply("Successfully joined the Channel")
+    except UserAlreadyParticipant:
+        return await message.reply("User is already a participant."
+    except (InviteHashInvalid, InviteHashExpired):
+        return await message.reply("Could not join. Maybe your link is expired or Invalid.")
+    except FloodWait:
+        return await message.reply("Too many requests, try again later.")
+    except Exception as e:
+        print(e)
+        return await message.reply("Could not join, try joining manually.")
+    
