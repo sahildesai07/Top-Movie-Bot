7df74db0415baee44b31dfb685e5ea554bf471e2
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-05-25 13:18:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -50,15 +50,6 @@ files = glob.glob(ppath)
 TechVJBot.start()
 loop = asyncio.get_event_loop()
 
-if SAVE_RESTRICTED_MODE == True:
-    tech_vj_userbot = Client("saverestricted", session_string=SESSION_STRING, api_hash=API_HASH, api_id=API_ID)
-    try:
-        tech_vj_userbot.start()
-    except BaseException:
-        print("Userbot Error ! Have you added SESSION while deploying??")
-        sys.exit(1)
-    temp.USERBOT = tech_vj_userbot
-
 telethon_bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
 async def start():
@@ -88,6 +79,14 @@ async def start():
     temp.ME = me.id
     temp.U_NAME = me.username
     temp.B_NAME = me.first_name
+    if SAVE_RESTRICTED_MODE == True:
+        tech_vj_userbot = Client("saverestricted", session_string=SESSION_STRING, api_hash=API_HASH, api_id=API_ID)
+        try:
+            tech_vj_userbot.start()
+        except BaseException:
+            print("Userbot Error ! Have you added SESSION while deploying??")
+            sys.exit(1)
+        temp.USERBOT = tech_vj_userbot
     logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
     logging.info(LOG_STR)
     logging.info(script.LOGO)
