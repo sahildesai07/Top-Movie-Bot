110cf7894092ed0b87d3b42e1d53d3aa4af45525
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-05-29 12:39:08 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -2,15 +2,14 @@
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-import os
+import os, logging, time
 from pyrogram import Client, filters, enums
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from info import IMDB_TEMPLATE
-from utils import extract_user, get_file_id, get_poster, last_online
-import time
+from utils import extract_user, get_file_id, get_poster, last_online 
 from datetime import datetime
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-import logging
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
