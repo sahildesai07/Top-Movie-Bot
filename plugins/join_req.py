b308653daa18674824b8dc4c6e5b7b27a441aaa3
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-05-30 12:59:04 +0530

    Update join_req.py

--- plugins/join_req.py
+++ plugins/join_req.py
@@ -1,26 +1,8 @@
-from logging import getLogger
 from pyrogram import Client, filters, enums
-from pyrogram.types import ChatJoinRequest
 from database.join_reqs import JoinReqs
-from info import ADMINS, AUTH_CHANNEL
+from info import ADMINS
 
 db = JoinReqs
-logger = getLogger(__name__)
-
-@Client.on_chat_join_request(filters.chat(AUTH_CHANNEL if AUTH_CHANNEL else "self"))
-async def join_reqs(client, join_req: ChatJoinRequest):
-    if db().isActive():
-        user_id = join_req.from_user.id
-        first_name = join_req.from_user.first_name
-        username = join_req.from_user.username
-        date = join_req.date
-
-        await db().add_user(
-            user_id=user_id,
-            first_name=first_name,
-            username=username,
-            date=date
-        )
 
 @Client.on_message(filters.command("totalrequests") & filters.private & filters.user(ADMINS))
 async def total_requests(client, message):
