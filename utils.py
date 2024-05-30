bc5a84476a93022901a6e47b6f6bd04d180cb4aa
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-05-30 13:16:36 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -69,6 +69,15 @@ async def is_subscribed(bot, query):
                 return True    
         except Exception as e:
             logger.exception(e)
+            try:
+                user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
+            except UserNotParticipant:
+                pass
+            except Exception as e:
+                logger.exception(e)
+            else:
+                if user.status != enums.ChatMemberStatus.BANNED:
+                    return True
             return False
     else:
         try:
