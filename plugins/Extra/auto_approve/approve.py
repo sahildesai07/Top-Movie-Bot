41572736cd4556ea48219ac57ca14246ad20d5ed
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-06-04 11:57:55 +0530

    Update approve.py

--- plugins/Extra/auto_approve/approve.py
+++ plugins/Extra/auto_approve/approve.py
@@ -8,15 +8,19 @@ join_db = JoinReqs
 
 @Client.on_chat_join_request((filters.group | filters.channel))
 async def auto_approve(client, message: ChatJoinRequest):
-    if message.chat.id == AUTH_CHANNEL:
-        if join_db().isActive():
-            user_id = message.from_user.id
-            first_name = message.from_user.first_name
-            username = message.from_user.username
-            date = message.date
-            await join_db().add_user(user_id=user_id, first_name=first_name, username=username, date=date)
-            data = await db.get_msg_command(user_id)
-            return 
+    if message.chat.id == AUTH_CHANNEL and join_db().isActive():
+        user_id = message.from_user.id
+        first_name = message.from_user.first_name
+        username = message.from_user.username
+        date = message.date
+        await join_db().add_user(user_id=user_id, first_name=first_name, username=username, date=date)
+        data = await db.get_msg_command(user_id)
+        try:
+            pre, file_id = data.split('_', 1)
+        except:
+            file_id = data
+            pre = ""
+        return 
     if AUTO_APPROVE_MODE == True:
         if not await db.is_user_exist(message.from_user.id):
             await db.add_user(message.from_user.id, message.from_user.first_name)
