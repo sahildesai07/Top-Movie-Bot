b7407b1ce7f506ed882466581b1bcfb15b2fb6ab
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-06-11 13:48:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -16,8 +16,8 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'PhdLust')
-API_ID = int(environ.get('API_ID', '22182189'))
-API_HASH = environ.get('API_HASH', '5e7c4088f8e23d0ab61e29ae11960bf5')
+API_ID = int(environ.get('API_ID', '22505271'))
+API_HASH = environ.get('API_HASH', 'c89a94fcfda4bc06524d0903977fc81e')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
