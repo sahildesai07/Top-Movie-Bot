b8c7c507b7d91807d88a1be4fcceab14e18fdc0e
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-06-11 12:18:26 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -17,7 +17,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("BenFilterBot")
+    return web.json_response("ultroidxTeam")
 
 @routes.get(r"/watch/{path:\S+}", allow_head=True)
 async def stream_handler(request: web.Request):
