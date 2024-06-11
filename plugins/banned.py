e6391697041d731ab6d28cbad5dbbac80e5de48f
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-06-11 12:13:21 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -1,6 +1,5 @@
 # Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+
 
 from pyrogram import Client, filters
 from utils import temp
