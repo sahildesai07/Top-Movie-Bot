cdd3fd64312fff0d31244c5f3453c47948169a45
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-06-27 17:50:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -69,7 +69,7 @@ async def start():
     today = date.today()
     now = datetime.now(tz)
     time = now.strftime("%H:%M:%S %p")
-    await dinesh12777Bot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
+    await PhdLustBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
     app = web.AppRunner(await web_server())
     await app.setup()
     bind_address = "0.0.0.0"
