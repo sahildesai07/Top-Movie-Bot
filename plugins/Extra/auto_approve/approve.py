fa20a1a2f6a46ece7e15f050b8d2b2a3044aefc8
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-06-27 16:39:43 +0530

    Update approve.py

--- plugins/Extra/auto_approve/approve.py
+++ plugins/Extra/auto_approve/approve.py
@@ -10,7 +10,7 @@ from info import *
 from utils import get_settings, pub_is_subscribed, get_size, is_subscribed, save_group_settings, temp, verify_user, check_token, check_verification, get_token, get_shortlink, get_tutorial, get_seconds
 from database.connections_mdb import active_connection
 from urllib.parse import quote_plus
-from dinesh12777.util.file_properties import get_name, get_hash, get_media_file_size
+from PhdLust.util.file_properties import get_name, get_hash, get_media_file_size
 logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
